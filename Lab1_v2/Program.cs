15cc65e70d9d171d84a9a0d56879a8f1e0732c58
Stephan <kochurov05@mail.ru>
2024-09-27 22:43:25 +0300

    update commands file, add some Exception in file Programm.cs

--- Lab1_v2/Program.cs
+++ Lab1_v2/Program.cs
@@ -34,66 +34,93 @@ internal class Program
 
 
         Console.WriteLine("-------Welcome to the TURTLEGAME-------");
+        Console.WriteLine();
         Console.WriteLine("Command list: \n" +
-            "- move\n" +
-            "- angle\n" + 
+            "- move [number]\n" +
+            "- angle [number]\n" + 
             "- penup\n" +
             "= pendown\n" + 
             "- history\n" + 
             "- listfigures\n" + 
-            "- color\n" + 
-            "- width");
+            "- color [string]\n" + 
+            "- width [number]");
         Console.WriteLine();
         Console.WriteLine("Choose the command from list to START the game");
 
 
         while (true)
         {
-            text = reader.Read();
-
-            if (text == "exit")
+            try
             {
-                break;
+                text = reader.Read();
+
+                if (text == "exit")
+                {
+                    break;
+                }
+
+                else if (text == "history")
+                {
+                    ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
+                    Invoker.Invoke(command);
+                    storageWriter.SaveCommand(text);
+
+                }
+
+                else if (text == "listfigures")
+                {
+                    ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
+                    Invoker.Invoke(command);
+                }
+
+                else if (commWithoutArgsList.Contains(text))
+                {
+
+                    ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
+                    Invoker.Invoke(command);
+                    storageWriter.SaveCommand(text);
+
+                    Console.WriteLine(notificator.SendCondition(turtle));
+
+                }
+
+                else
+                {
+                    ICommandsWithArgs command = (ICommandsWithArgs)manager.DefineCommand(text.Split(' ')[0]);
+                    Invoker.Invoke(command, text.Split(' ')[1]);
+                    storageWriter.SaveCommand(text);
+                    Console.WriteLine(notificator.SendCondition(turtle));
+                }
+                //else
+                //{
+                //    Console.WriteLine("command doesn`t exist");
+                //}
+
+                checker.Check();
             }
 
-            else if (text == "history")
+            catch (InvalidCastException ex)
             {
-                ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
-                Invoker.Invoke(command);
-                storageWriter.SaveCommand(text);
-               
+                Console.WriteLine("Invalid argument");
             }
 
-            else if (text == "listfigures")
+            //catch (ArgumentException ex)
+            //{
+            //    Console.WriteLine("Invalid argument");
+            //}
+
+            catch (KeyNotFoundException ex)
             {
-                ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
-                Invoker.Invoke(command);
+                Console.WriteLine("Invalid command, or command doesn`t exist");
             }
 
-            else if (commWithoutArgsList.Contains(text))
+            catch (FormatException ex)
             {
-                ICommandsWithoutArgs command = (ICommandsWithoutArgs)manager.DefineCommand(text);
-                Invoker.Invoke(command);
-                storageWriter.SaveCommand(text);
-                
-                Console.WriteLine(notificator.SendCondition(turtle));
-
+                Console.WriteLine("Invalid argument, please try again or check command list");
             }
 
-            else if (commWithArgsList.Contains(text.Split(' ')[0]))
-            {
-                ICommandsWithArgs command = (ICommandsWithArgs)manager.DefineCommand(text.Split(' ')[0]);
-                Invoker.Invoke(command, text.Split(' ')[1]);
-                storageWriter.SaveCommand(text);
-                Console.WriteLine(notificator.SendCondition(turtle));
-            }
-            else
-            {
-                Console.WriteLine("command doesn`t exist");
-            } 
-            
-            checker.Check();
-            
+
+
         }
 
         Console.WriteLine("GAME END");
